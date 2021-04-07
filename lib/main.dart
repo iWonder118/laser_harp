1353d87ee8a012298071da0ab8223ac6a05bb3a7
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2021-04-07 23:32:58 +0900

    flutter plactice extended default

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,77 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final title = "Flutterサンプル";
-  final message = "サンプル・メッセージ";
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: new MyHomePage(
-        title: this.title,
-        message: this.message),
-    );
+        title: 'Flutter Demo',
+        theme: new ThemeData(primarySwatch: Colors.blue),
+        home: new MyHomePage(title: "Flutterサンプル"));
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
-  final String message;
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-  MyHomePage({this.title, this.message}): super();
+  final String title;
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incremetCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decremetCounter() {
+    setState(() {
+      _counter--;
+      if (_counter <= 0) _counter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Text(
-        widget.message,
-        style: TextStyle(fontSize: 32.0),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("You have pushed the button this many times:"),
+            Text(
+              "$_counter",
+              style: Theme.of(context).textTheme.display1,
+            )
+          ],
+        ),
       ),
+      floatingActionButton: Column(
+        verticalDirection: VerticalDirection.down,
+        mainAxisSize:  MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: _incremetCounter,
+            tooltip: "Increment",
+            child: Icon(Icons.add),
+            backgroundColor: Colors.red[500]
+          ),
+          Padding(padding: EdgeInsets.symmetric(vertical: 5.0)),
+          FloatingActionButton(
+            onPressed: _decremetCounter,
+            tooltip: "Decrement",
+            child: Icon(Icons.remove),
+            backgroundColor: Colors.blue[500],
+          ),
+        ],
+      ) 
     );
   }
 }
