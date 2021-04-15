31d2adf73ff65d5bfc1079f8167ca9d5c6a55985
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2021-04-16 00:09:20 +0900

    add sound play

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 import "dart:math";
+// インポートするライブラリ
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: new ThemeData(primarySwatch: Colors.blue),
-        home: new TopPage(title: "Flutterサンプル"));
+        home: new TopPage(title: "レーザーハープDEMO"));
   }
 }
 
@@ -25,8 +28,16 @@ class TopPage extends StatefulWidget {
 }
 
 class _TopPageState extends State<TopPage> {
-  double _currentPositionX = 200;
+  GlobalKey globalKey = GlobalKey();
+  double _currentPositionX = 0;
   double _currentPositionY = 0;
+  double lazerWidth1 = 1000;
+  double lazerWidth2 = 1000;
+  double lazerWidth3 = 1000;
+  bool islazer1Played = false;
+  bool islazer2Played = false;
+  bool islazer3Played = false;
+  AudioCache _player = AudioCache();
 
   @override
   Widget build(BuildContext context) {
@@ -48,8 +59,26 @@ class _TopPageState extends State<TopPage> {
                 ],
               ),
             ),
+            Container(
+              width: lazerWidth1,
+              height: 10,
+              margin: EdgeInsets.fromLTRB(10, 100, 10, 100),
+              color: Colors.greenAccent[400],
+            ),
+            Container(
+              width: lazerWidth2,
+              height: 10,
+              margin: EdgeInsets.fromLTRB(10, 310, 10, 100),
+              color: Colors.greenAccent[400],
+            ),
+            Container(
+              width: lazerWidth3,
+              height: 10,
+              margin: EdgeInsets.fromLTRB(10, 520, 10, 100),
+              color: Colors.greenAccent[400],
+            ),
             GestureDetector(
-              onPanUpdate: (DragUpdateDetails details) {
+              onPanUpdate: (DragUpdateDetails details) async {
                 _currentPositionX = max(
                     0,
                     min(details.localPosition.dx,
@@ -58,13 +87,48 @@ class _TopPageState extends State<TopPage> {
                     0,
                     min(details.localPosition.dy,
                         MediaQuery.of(context).size.height));
-
+                lazerWidth1 =
+                    (100 <= _currentPositionY && _currentPositionY <= 110)
+                        ? _currentPositionX - 10
+                        : MediaQuery.of(context).size.width;
+                lazerWidth2 =
+                    (310 <= _currentPositionY && _currentPositionY <= 320)
+                        ? _currentPositionX - 10
+                        : MediaQuery.of(context).size.width;
+                lazerWidth3 =
+                    (520 <= _currentPositionY && _currentPositionY <= 530)
+                        ? _currentPositionX - 10
+                        : MediaQuery.of(context).size.width;
+                if (100 <= _currentPositionY && _currentPositionY <= 110) {
+                  if (!islazer1Played) {
+                    _player.play('sound1.mp3');
+                    islazer1Played = true;
+                    await Future.delayed(Duration(seconds: 1));
+                    islazer1Played = false;
+                  }
+                }
+                if (310 <= _currentPositionY && _currentPositionY <= 320) {
+                  if (!islazer2Played) {
+                    _player.play('sound2.mp3');
+                    islazer2Played = true;
+                    await Future.delayed(Duration(seconds: 1));
+                    islazer2Played = false;
+                  }
+                }
+                if (520 <= _currentPositionY && _currentPositionY <= 530) {
+                  if (!islazer3Played) {
+                    _player.play('sound3.mp3');
+                    islazer3Played = true;
+                    await Future.delayed(Duration(seconds: 1));
+                    islazer3Played = false;
+                  }
+                }
                 setState(() {});
               },
               child: Container(
-                  width: _currentPositionX,
-                  height: 10,
-                  color: Colors.greenAccent[400]),
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.transparent),
             ),
           ],
         ));
