9a5a72994494cc21af7eba58b4d27f2eedb7b88e
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2021-04-19 23:22:50 +0900

    refact cross line event

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class _TopPageState extends State<TopPage> {
   GlobalKey globalKey = GlobalKey();
   double _currentPositionX = 0;
   double _currentPositionY = 0;
+  double lazerThickness = 5;
   double lazerWidth1 = 1000;
   double lazerWidth2 = 1000;
   double lazerWidth3 = 1000;
@@ -61,24 +62,24 @@ class _TopPageState extends State<TopPage> {
             ),
             Container(
               width: lazerWidth1,
-              height: 10,
+              height: 5,
               margin: EdgeInsets.fromLTRB(10, 100, 10, 100),
-              color: Colors.greenAccent[400],
+              color: Colors.greenAccent[700],
             ),
             Container(
               width: lazerWidth2,
-              height: 10,
+              height: 5,
               margin: EdgeInsets.fromLTRB(10, 310, 10, 100),
-              color: Colors.greenAccent[400],
+              color: Colors.greenAccent[700],
             ),
             Container(
               width: lazerWidth3,
-              height: 10,
+              height: 5,
               margin: EdgeInsets.fromLTRB(10, 520, 10, 100),
-              color: Colors.greenAccent[400],
+              color: Colors.greenAccent[700],
             ),
             GestureDetector(
-              onPanUpdate: (DragUpdateDetails details) async {
+              onPanUpdate: (DragUpdateDetails details) {
                 _currentPositionX = max(
                     0,
                     min(details.localPosition.dx,
@@ -87,42 +88,12 @@ class _TopPageState extends State<TopPage> {
                     0,
                     min(details.localPosition.dy,
                         MediaQuery.of(context).size.height));
-                lazerWidth1 =
-                    (100 <= _currentPositionY && _currentPositionY <= 110)
-                        ? _currentPositionX - 10
-                        : MediaQuery.of(context).size.width;
-                lazerWidth2 =
-                    (310 <= _currentPositionY && _currentPositionY <= 320)
-                        ? _currentPositionX - 10
-                        : MediaQuery.of(context).size.width;
-                lazerWidth3 =
-                    (520 <= _currentPositionY && _currentPositionY <= 530)
-                        ? _currentPositionX - 10
-                        : MediaQuery.of(context).size.width;
-                if (100 <= _currentPositionY && _currentPositionY <= 110) {
-                  if (!islazer1Played) {
-                    _player.play('sound1.mp3');
-                    islazer1Played = true;
-                    await Future.delayed(Duration(seconds: 1));
-                    islazer1Played = false;
-                  }
-                }
-                if (310 <= _currentPositionY && _currentPositionY <= 320) {
-                  if (!islazer2Played) {
-                    _player.play('sound2.mp3');
-                    islazer2Played = true;
-                    await Future.delayed(Duration(seconds: 1));
-                    islazer2Played = false;
-                  }
-                }
-                if (520 <= _currentPositionY && _currentPositionY <= 530) {
-                  if (!islazer3Played) {
-                    _player.play('sound3.mp3');
-                    islazer3Played = true;
-                    await Future.delayed(Duration(seconds: 1));
-                    islazer3Played = false;
-                  }
-                }
+                lazerWidth1 = judgeCrossLine(100);
+                lazerWidth2 = judgeCrossLine(310);
+                lazerWidth3 = judgeCrossLine(520);
+                plyaSound(100, "sound1");
+                plyaSound(310, "sound2");
+                plyaSound(520, "sound3");
                 setState(() {});
               },
               child: Container(
@@ -132,5 +103,24 @@ class _TopPageState extends State<TopPage> {
             ),
           ],
         ));
+  }
+
+  double judgeCrossLine(double startY) {
+    return (startY <= _currentPositionY &&
+            _currentPositionY <= startY + lazerThickness)
+        ? _currentPositionX - 10
+        : MediaQuery.of(context).size.width;
+  }
+
+  void plyaSound(double startY, String fileName) async {
+    if (startY <= _currentPositionY &&
+        _currentPositionY <= startY + lazerThickness) {
+      if (!islazer1Played) {
+        _player.play('$fileName.mp3');
+        islazer1Played = true;
+        await Future.delayed(Duration(seconds: 1));
+        islazer1Played = false;
+      }
+    }
   }
 }
